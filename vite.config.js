53cdbb7b91e58ff20a6ab7919f40ff9f300f5460
KingOfNorth <mike.fury315@gmail.com>
2026-04-29 23:08:52 +0700

    Fix base path

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/flot-rt/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
